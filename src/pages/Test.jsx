92bc0649c53e0dde018d736d5703c396b590f7e4
tzu <wishcolor0770@yahoo.com.tw>
2022-07-29 17:05:25 +0800

    status

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -11,7 +11,7 @@ const TabContent0 = () => <div className="content">Content0</div>;
 const TabContent1 = () => <div className="content">Content1</div>;
 const TabContent2 = () => <div className="content">Content2</div>;
 
-const BUTTONS = ["Content0", "Content1", "Content2"];
+const BUTTONS = [ "Content0","Content1","Content2",];
 
 
 
@@ -20,19 +20,33 @@ const Test = () => {
 
 
   const [click, setclick] = useState(null);
+  const [choose, setChoose] = useState(null);
+
+
+  let nowBtn = {
+    now:choose,
+    buttons:[...BUTTONS]
+  }
 
   const handleClick = (event, key) => {
-    console.log('key index: ', key);
+
+    // console.log(event);
+    // console.log('key index: ', key);
 
     if (key == 0) {
       setclick(TabContent0);
+      setChoose(0);
+
     } else if (key == 1) {
       setclick(TabContent1);
+      setChoose(1);
     } else {
       setclick(TabContent2);
+      setChoose(2);
     }
 
   };
+
 
 
 
@@ -40,9 +54,11 @@ const Test = () => {
     <div>
 
       <div>
-        {BUTTONS.map((element, key) => (
-          <Button type="primary" shape="round" size="large" onClick={event => handleClick(event, key)} key={key}>{element}</Button>
-        ))}
+        {BUTTONS.map((element, key) => {
+          return (
+            <Button type={key==choose?"primary":"dashed"} shape="round" size="large" onClick={event => handleClick(event, key)} key={key}>{element}</Button>
+          );
+        })}
       </div>
 
       <div>
